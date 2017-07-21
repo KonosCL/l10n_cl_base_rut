9f50acee7d61bc960ddd78b3bb4d33be9ecabf5d
Nelson Ramírez Sánchez <info@nelsonramirez.com>
2017-07-21 17:45:08 -0400

    Update From Original

--- base_vat.py
+++ base_vat.py
@@ -2,13 +2,18 @@
 from odoo import models, fields, api
 import re
 
+
 class res_partner(models.Model):
     _inherit = 'res.partner'
 
-    def check_vat_cl(self, vat):
+    formated_vat = fields.Char(
+        translate=True, string='Printable VAT', store=True,
+        help='Show formatted vat')
+
+    def check_vat_cl (self, vat):
         body, vdig = '', ''
-        if vat == '555555555':
-            return True
+        if len(vat) > 9:
+            vat = vat.replace('-','',1).replace('.','',2)
         if len(vat) != 9:
             return False
         else:
@@ -24,3 +29,18 @@ class res_partner(models.Model):
                 return False
         except IndexError:
             return False
+
+    @api.onchange('formated_vat')
+    def onchange_document(self):
+        mod_obj = self.env['ir.model.data']
+        formated_vat = (
+            re.sub('[^1234567890Kk]', '',
+            str(self.formated_vat))).zfill(9).upper()
+
+        self.vat = 'CL%s' % formated_vat
+
+        
+        self.formated_vat = '%s.%s.%s-%s' % (
+            formated_vat[0:2], formated_vat[2:5], formated_vat[5:8],
+            formated_vat[-1])
+
